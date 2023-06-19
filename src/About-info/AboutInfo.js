8694b9e18d64a78e957167b7c58d5f237c701b5a
ThananyaSih <thananya.sih@gmail.com>
2023-06-19 19:51:55 +0700

    base code

---
+++ src/About-info/AboutInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutInfo = () => {
+  return (
+    <div>
+      <div>About Page</div>
+      <link href="/Home-Page" />
+    </div>
+  );
+};
+
+export default AboutInfo;
---
+++ src/Home-page/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <div>Home Page</div>
+        <link href="/About-info" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
