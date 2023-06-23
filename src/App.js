45fc439c762fa6c4a318165decd5b4a2b5ae934e
ThananyaSih <thananya.sih@gmail.com>
2023-06-23 15:20:59 +0700

    manage folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import NavbarElement from "./components/Navbar/NavbarElements";
-import mainLogo from "./mainLogo.png";
+import mainLogo from "./img/mainLogo.png";
 
 function App() {
   return (
