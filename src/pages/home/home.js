1f12083e7d2c25ed441fb67059ab549db33523c0
ThananyaSih <thananya.sih@gmail.com>
2023-06-29 22:20:17 +0700

    edit page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,7 +5,15 @@ const Home = () => {
   return (
     <div>
       <div className="home-container">
-        <h1>Home Page</h1>
+        <h1>Create Better Place</h1>
+        <h1>Space for Plantism.</h1>
+      </div>
+      <div className="img-container">
+        <img src="src/img/homePlant01.jpg" alt="home01"></img>
+        <img src="src/img/homePlant02.jpg" alt="home02"></img>
+        <img src="src/img/homePlant03.jpg" alt="home03"></img>
+        <img src="src/img/homePlant04.jpg" alt="home04"></img>
+        <img src="src/img/homePlant05.jpg" alt="home05"></img>
       </div>
     </div>
   );
