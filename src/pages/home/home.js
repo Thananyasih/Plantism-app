55ced7f219f3999ead5c22e99496ff73ba5efa92
ThananyaSih <thananya.sih@gmail.com>
2023-07-03 16:24:02 +0700

    edit styles

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,20 +1,24 @@
 import React from "react";
 import "./home.css";
-import homePlant01 from ".../assets/homePlant01.jpg";
+// import homePlant01 from "../../assets/homePlant01.jpg";
+import homePlant02 from "../../assets/homePlant02.jpg";
+import homePlant03 from "../../assets/homePlant03.jpg";
+import homePlant04 from "../../assets/homePlant04.jpg";
+import homePlant05 from "../../assets/homePlant05.jpg";
 
 const Home = () => {
   return (
     <div>
       <div className="home-container">
-        <h1>Create Better Place</h1>
-        <h3>The Space for Plantism.</h3>
+        <h1>Create Nature Space</h1>
+        {/* <h3>- The place for Plantism -</h3> */}
       </div>
       <div className="img-container">
-        <img src={homePlant01} alt="home01"></img>
-        <img src="./img/homePlant02.jpg" alt="home02"></img>
-        <img src="./img/homePlant03.jpg" alt="home03"></img>
-        <img src="./img/homePlant04.jpg" alt="home04"></img>
-        <img src="./img/homePlant05.jpg" alt="home05"></img>
+        {/* <img className="img-plant" src={homePlant01} alt="home01"></img> */}
+        <img className="img-plant" src={homePlant02} alt="home02"></img>
+        <img className="img-plant" src={homePlant03} alt="home03"></img>
+        <img className="img-plant" src={homePlant04} alt="home04"></img>
+        <img className="img-plant" src={homePlant05} alt="home05"></img>
       </div>
     </div>
   );
