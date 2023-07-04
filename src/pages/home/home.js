0fcd959d15c12ebf937b075218cbdc52aa7cb569
ThananyaSih <thananya.sih@gmail.com>
2023-07-04 16:47:07 +0700

    edit home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./home.css";
-// import homePlant01 from "../../assets/homePlant01.jpg";
+import homePlant01 from "../../assets/homePlant01.jpg";
 import homePlant02 from "../../assets/homePlant02.jpg";
 import homePlant03 from "../../assets/homePlant03.jpg";
 import homePlant04 from "../../assets/homePlant04.jpg";
@@ -14,25 +14,28 @@ const Home = () => {
         <h3>Select the place for fast ideas !</h3>
       </div>
       <div className="img-container">
-        {/* <img className="img-plant" src={homePlant01} alt="home01"></img> */}
+        <div className="gallery">
+          <img className="img-plant" src={homePlant01} alt="home01"></img>
+          <div className="desc">Tips from Planter</div>
+        </div>
         <div className="gallery">
           <img className="img-plant" src={homePlant02} alt="home02"></img>
-          <div className="desc">About home 01</div>
+          <div className="desc">Plant Styles</div>
         </div>
 
         <div className="gallery">
           <img className="img-plant" src={homePlant03} alt="home03"></img>
-          <div className="desc">About home 02</div>
+          <div className="desc">Outdoor Plants</div>
         </div>
 
         <div className="gallery">
           <img className="img-plant" src={homePlant04} alt="home04"></img>
-          <div className="desc">About home 03</div>
+          <div className="desc">Indoor Plants</div>
         </div>
 
         <div className="gallery">
           <img className="img-plant" src={homePlant05} alt="home05"></img>
-          <div className="desc">About home 04</div>
+          <div className="desc">Customize Own Space</div>
         </div>
       </div>
     </div>
