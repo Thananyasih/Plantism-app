c1027445fdd2a8c9cb87dd9c635c5776401192e7
ThananyaSih <thananya.sih@gmail.com>
2023-07-12 13:01:31 +0700

    edit footer

--- src/components/Footer/FooterBar.jsx
+++ src/components/Footer/FooterBar.jsx
@@ -8,7 +8,13 @@ const FooterBar = () => {
     <div>
       <div>
         <div className="footer-container">
-          <h3>Plantism</h3>
+          <h3 id="head">Plantism</h3>
+          <h4 id="madeBy">create by : Thananys S.</h4>
+          <div className="contact-container">
+            <h3 id="contact">Contact</h3>
+            <li>E-mail :</li>
+            <li>GitHub :</li>
+          </div>
         </div>
         <img src={mainLogo} alt="logo" className="img-logo" />
       </div>
