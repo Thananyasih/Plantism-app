e9af4deeaeeb074117e53663a8d0aa172fbbe5f5
ThananyaSih <thananya.sih@gmail.com>
2023-07-12 10:11:50 +0700

    edit about

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -5,7 +5,12 @@ const About = () => {
   return (
     <div>
       <div className="about-container">
-        <h1>About Page</h1>
+        <h1>Why Plantism ?</h1>
+        <h3>
+          This web page is collection for planter who aims to find plants breed
+          and tips from community, also need ideas to create and planning own
+          nature space.
+        </h3>
       </div>
     </div>
   );
