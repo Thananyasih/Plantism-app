921a2fe70b7876f05e08da95d076155f2ed4bce1
ThananyaSih <thananya.sih@gmail.com>
2023-07-12 15:00:31 +0700

    edit blogs

--- src/pages/blogs/blogs.js
+++ src/pages/blogs/blogs.js
@@ -5,50 +5,64 @@ const Blogs = () => {
   return (
     <div>
       <div className="blogs-container">
-        <div className="top-planter">
+        <div className="top-blogs">
           <h1>Popular plants blogs 🪴</h1>
-          <div className="popular-container">
-            <li>1. The Houseplants Guru</li>
-            <h3>#1 most popular plants blogs</h3>
-            <a href="https://feedly.com/i/subscription/feed%2Fhttp%3A%2F%2Fwww.thehouseplantguru.com%2F%3Ffeed%3Drss2">
+
+          <div className="pop-1">
+            <button
+              className="pop-button"
+              href="https://feedly.com/i/subscription/feed%2Fhttp%3A%2F%2Fwww.thehouseplantguru.com%2F%3Ffeed%3Drss2"
+            >
               Read more &rarr;
-            </a>
+            </button>
+            <li className="pop-li">1. The Houseplants Guru</li>
+            <h3 className="pop-desc">
+              # We all need a little green in our lives.
+            </h3>
           </div>
 
-          <div className="popular-container">
-            <li>2. Houseplant Care Tips</li>
-            <h3>#2 most popular plants blogs</h3>
-
-            <a href="https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fohiotropics.com%2Ffeed%2F">
+          <div className="pop-2">
+            <button
+              className="pop-button"
+              href="https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fohiotropics.com%2Ffeed%2F"
+            >
               Read more &rarr;
-            </a>
+            </button>
+            <li className="pop-li">2. Houseplant Care Tips</li>
+            <h3 className="pop-desc">#2 most popular plants blogs</h3>
           </div>
 
-          <div className="popular-container">
-            <li>3. Cactus Blog - Cactus Jungle</li>
-            <h3>#3 most popular plants blogs</h3>
-
-            <a href="https://feedly.com/i/subscription/feed%2Fhttp%3A%2F%2Fwww.cactusjungle.com%2Fblog%2Ffeed%2F">
+          <div className="pop-3">
+            <button
+              className="pop-button"
+              href="https://feedly.com/i/subscription/feed%2Fhttp%3A%2F%2Fwww.cactusjungle.com%2Fblog%2Ffeed%2F"
+            >
               Read more &rarr;
-            </a>
+            </button>
+            <li className="pop-li">3. Cactus Blog - Cactus Jungle</li>
+            <h3 className="pop-desc">#3 most popular plants blogs</h3>
           </div>
 
-          <div className="popular-container">
-            <li>4. Clever Bloom</li>
-            <h3>#4 most popular plants blogs</h3>
-
-            <a href="https://feedly.com/i/subscription/feed%2Fhttp%3A%2F%2Fcleverbloom.com%2Ffeed%2F">
+          <div className="pop-4">
+            <button
+              className="pop-button"
+              href="https://feedly.com/i/subscription/feed%2Fhttp%3A%2F%2Fcleverbloom.com%2Ffeed%2F"
+            >
               Read more &rarr;
-            </a>
+            </button>
+            <li className="pop-li">4. Clever Bloom</li>
+            <h3 className="pop-desc">#4 most popular plants blogs</h3>
           </div>
 
-          <div className="popular-container">
-            <li>5. Bloomscape</li>
-            <h3>#5 most popular plants blogs</h3>
-
-            <a href="https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fbloomscape.com%2Ffeed%2F">
+          <div className="pop-5">
+            <button
+              className="pop-button"
+              href="https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fbloomscape.com%2Ffeed%2F"
+            >
               Read more &rarr;
-            </a>
+            </button>
+            <li className="pop-li">5. Bloomscape</li>
+            <h3 className="pop-desc">#5 most popular plants blogs</h3>
           </div>
         </div>
       </div>
