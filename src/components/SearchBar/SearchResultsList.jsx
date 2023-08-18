373b72f2ef991e93d9c4fea8cc48ab8fc28861d8
ThananyaSih <thananya.sih@gmail.com>
2023-08-18 22:33:43 +0700

    edit api

--- src/components/SearchBar/SearchResultsList.jsx
+++ src/components/SearchBar/SearchResultsList.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "./SearchResultsList.css";
 
-export const SearchResultsList = () => {
-  return <div>SearchResultsList</div>;
+export const SearchResultsList = ({ results }) => {
+  return (
+    <div className="results-list">
+      {results.map((result, id) => {
+        return <div key={id}>{result.data.common_name}</div>;
+      })}
+    </div>
+  );
 };
