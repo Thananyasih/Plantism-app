9054b672816a9d9f5041ce928e24ffac3400be5d
Thananyasih <thananya.sih@gmail.com>
2023-09-07 15:22:28 +0700

    edit

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,46 +1,21 @@
 // key: sk-PmRY64d7a7e30c48c1856
+// "https://perenual.com/api/species-list?page=1&key=sk-PmRY64d7a7e30c48c1856"
 
-import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import Axios from "axios";
-
 import "./SearchBar.css";
-// import { json } from "react-router-dom";
 
-const SearchBar = ({ setResults }) => {
-  const [input, setInput] = useState("");
-  
-  const fetchData = (value) => {
-    // fetch(
-    //   "https://perenual.com/api/species-list?page=1&key=sk-PmRY64d7a7e30c48c1856"
-    // )
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //   });
+const SearchBar = () => {
 
-    Axios.get(
-      "https://perenual.com/api/species-list?page=1&key=sk-PmRY64d7a7e30c48c1856"
-    ).then((res) => {
-      console.log(res.data.data);
-    });
-  };
+  return (  
+      <div className="search" >
+        <input
+          type="text"
+          placeholder="Search Here..."
+          />
+          {<FaSearch id="search-icon" />}
+      </div>
 
-  const handleChange = (value) => {
-    setInput(value);
-    fetchData(value);
-  };
-
-  return (
-    <div className="input-wrapper">
-      <FaSearch id="search-icon" />
-      <input
-        placeholder="Type to search..."
-        value={input}
-        onChange={(e) => handleChange(e.target.value)}
-      />
-    </div>
-  );
+  )
 };
 
 export default SearchBar;
--- src/pages/plantalog/plantalog.js
+++ src/pages/plantalog/plantalog.js
@@ -1,26 +1,13 @@
-import React, { useState } from "react";
-import "./plantalog.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
-import { SearchResultsList } from "../../components/SearchBar/SearchResultsList";
+import "./plantalog.css";
 
 const Plantalog = () => {
-  const [results, setResults] = useState([]);
 
   return (
-    <div >
-      <div className="plantalog-container">
-      <h2>Search Plants...</h2>
-      </div>
-      <div className="search-bar-container">
-        <SearchBar setResults={setResults} />
-        {results && results.length > 0 && (
-          <SearchResultsList results={results} />
-        )}
-      </div>
-      <h3>Filter Plants</h3>
-
+    <div className="wrapper">
+      <SearchBar></SearchBar>
     </div>
-  );
+  )
 };
 
 export default Plantalog;
