9556ae8241b4cfdb3ab3889926c9f0ebfc735224
Thananyasih <thananya.sih@gmail.com>
2023-09-14 23:13:26 +0700

    edit search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -11,40 +11,59 @@ const SearchBar = () => {
 
   const [search, setSearch] = useState('')
   const [plantList, setPlantList] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
+  const [headChange, setHeadChange] = useState('')
 
   useEffect(() => {
     axios.get('https://perenual.com/api/species-list?&key=sk-7psF64f8528c3e1441856')
     .then((res) => {
       setPlantList(res.data.data)
     })
-  },[])
-    
+  },[search])   
 
-  const handleSearchFilter = (e) => {
-    setSearch(e.target.value)
+  const handleClicked = (e) => {
+    e.preventDefault();
+    setIsClicked(isClicked => !isClicked);
+    if(!isClicked) (
+      <div className="search-result-head" style={{color: 'red'}}>This is {search.e.target.value} details!</div>
+    );
   }
 
   return (  
     <div className="search-top">
         <div className="search" >
             {<FaSearch id="search-icon" />}
-          <div >
+          <div>
             <input
               type="text"
               placeholder="Search Plants Here..."
-              onChange={handleSearchFilter}
+              onChange={(e) => setSearch(e.target.value)}            
               />
+            <div className="search-result-container">
               {plantList.map((item) => {
-                if(search === ''){
-                  return <div key={item.id} className="search-result" style={{display:'none'}}/>                   
-                } else if (item.common_name.toLowerCase().includes(search.toLowerCase())){
-                  return <div key={item.id} className="search-result" >
-                  <li>{item.common_name}</li>                  
-                  </div>
-                }
-              })}
-          </div>
+                return search === '' ? 
+                <div key={item.id} className='search-result' style={{display:'none'}}/> 
+                 : item.common_name.toLowerCase().startsWith(search.toLowerCase()) ? (                    
+                      <div key={item.id}>
+                        <div onClick={handleClicked} className='search-result'>{item.common_name}</div>                                        
+                      </div>                   
+                      ) : ''
+                }               
+              )}
+            </div>
+            <div className="search-result-head" onChange={(e) => setHeadChange(e.target.value)}>This is {search} details!</div>
+          </div>             
+          
         </div>
+        <div>
+            <h2>Indoor Plant Recommended</h2>
+          </div>
+          <div>
+            <h2>Outdoor Plant Recommended</h2>
+          </div>
+          <div>
+            <h2>In-door Plant Recommended</h2>
+          </div>
     </div>
   )
 };
