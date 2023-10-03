a693a73f4fa206cf456ef409f525be27ab40c1fc
Thananyasih <thananya.sih@gmail.com>
2023-10-03 23:22:15 +0700

    make search card

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,34 +1,63 @@
 // key: sk-7psF64f8528c3e1441856
 // "https://perenual.com/api/species-list?page=1&key=sk-7psF64f8528c3e1441856"
 
-import { FaSearch } from "react-icons/fa";
-import "./SearchBar.css";
 import { useEffect, useState } from "react";
-import axios from "axios";
-import PlantsCard from "./PlantsCard";
+
+import { FaSearch } from "react-icons/fa";
+// import axios from "axios";
+import "./SearchBar.css";
+// import PlantsCard from "./PlantsCard";
 
 
 
 const SearchBar = () => {
 
   const [search, setSearch] = useState('')
-  const [plantSearch, setPlantSearch] = useState([]);
+  // const [plantSearch, setPlantSearch] = useState([]);
   // const [isClicked, setIsClicked] = useState(false);
-  // const [selected, setSelected] = useState([])
+  const [testData, setTestData] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://perenual.com/api/species-list?&key=sk-7psF64f8528c3e1441856')
+  // useEffect(() => {
+  //   axios.get('https://perenual.com/api/species-list?&key=sk-7psF64f8528c3e1441856')
+  //   .then((res) => {
+  //     setPlantSearch(res.data.data)
+  // console.log(plantSearch);
+  //   })
+  // },[search])
+
+  // useEffect(() => {
+  // fetch('https://perenual.com/api/species-list?&key=sk-7psF64f8528c3e1441856')
+  //   .then((res) => {
+  //     return res.json()})
+  //   .then(data => {
+  //       setPlantSearch(data)
+  //       console.log(data);
+  //       // console.log(selected);
+  //     }).catch((err) => console.error('ERR'))   
+  //   },[])
+
+// TEST with Fake API  
+useEffect(() => {
+  fetch('http://localhost:4000/data')
     .then((res) => {
-      setPlantSearch(res.data.data)
-    })
-  },[search])   
+      return res.json()})
+    .then(data => {
+        setTestData(data)
+        console.log(data);
+        // console.log(selected);
+      })   
+    },[])
+
+const filtered = !search ? testData : testData.filter((item) => (
+  item.common_name.toLowerCase().startsWith(search.toLowerCase())
+))
+
+// const filtered = !search ? plantSearch : plantSearch.filter((item) => (
+//   item.common_name.toLowerCase().startsWith(search.toLowerCase())
+// ))
 
 
-  // const handleClicked = (e) => {
-  //   e.preventDefault();
-  //   setIsClicked(isClicked => !isClicked);
-  // }
-
+  
   return (  
     <div className="search-top">
         <div className="search" >
@@ -37,34 +66,45 @@ const SearchBar = () => {
            
             <input
               type="text"
+              value={search}
               placeholder="Search Plants Here..."
               onChange={(e) => setSearch(e.target.value)}            
               />
-            <div className="search-result-container">
+            <div >
               
-              {plantSearch.map((item) => {
+              {/* {plantSearch.map((item) => {
                 return search === '' ? 
                 <div key={item.id} className='search-result' style={{display:'none'}} /> 
                  : item.common_name.toLowerCase().startsWith(search.toLowerCase()) ? (                    
                       <div key={item.id}>
-                        <div className='search-result'>{item.common_name}</div>                                        
+                        <div className='search-result' onClick={(e)=>{setSelected(e.target.value)}}>{item.common_name}</div>                                        
                       </div>                   
                       ) : ''
+                }               
+              )} */}
+
+              {filtered.map((item) => {
+                return search === '' ? 
+                <div key={item.id} className="search-result-container" style={{display:'none'}} /> 
+                 : (  <div key={item.id}>
+                        {/* <div className="search-result" onClick={handleClicked}>{item.common_name}</div>                                         */}
+                        <ul className="plant-cards">
+                          <img src={item.default_image.regular_url
+} alt={item.common_name} style={{width:'400px', height: 'auto'}}/>
+                          <p>
+                            <h2>{item.common_name}</h2>
+                            <h3>Scientific Name: {item.scientific_name}</h3>
+                            <h3>Cycle: {item.cycle}</h3>
+                            <h3>Watering: {item.watering}</h3>
+                          </p>
+                        </ul>                   
+                      </div>
+                      )
                 }               
               )}
             </div>
           </div> 
-        </div>
-        
-        <div>
-            <h2>Indoor Plant Recommended</h2>
-          </div>
-          <div>
-            <h2>Outdoor Plant Recommended</h2>
-          </div>
-          <div>
-            <h2>In-door Plant Recommended</h2>
-          </div>
+        </div>        
     </div>
   )
 };
