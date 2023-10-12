22ead69944dd1a62c3e10559c9874b603bdf1d24
Thananyasih <thananya.sih@gmail.com>
2023-10-12 18:04:11 +0700

    edit ui

--- src/pages/design/design.js
+++ src/pages/design/design.js
@@ -5,6 +5,8 @@ const Design = () => {
   return (
     <div>
       <div className="design-bg" />
+      <img src="https://wallpapercave.com/wp/wp6680363.jpg" alt="graphic-plant" className="graphic-plant"/>
+
       <div className="design-container">
         <h1>Design Space & Pottery</h1>
       </div>
