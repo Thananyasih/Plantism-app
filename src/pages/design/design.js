9734a0a65bed94eab84c833895374acfad168d6d
Thananyasih <thananya.sih@gmail.com>
2023-10-15 11:38:59 +0700

    edit ui

--- src/pages/design/design.js
+++ src/pages/design/design.js
@@ -4,24 +4,24 @@ import "./design.css";
 const Design = () => {
   return (
     <div>
-      <div className="design-bg" />
+      <div className="design-bg"></div>
       {/* <img src="https://wallpapercave.com/wp/wp6680363.jpg" alt="graphic-plant" className="graphic-plant"/> */}
 
       <div className="design-container">
         <h1>Plantism Design Space </h1>
-            <img src="https://cdn.mos.cms.futurecdn.net/6Kowst46WKuB7BzkPs7Pcn-1920-80.jpg.webp" alt="landscape zone"></img>
+            {/* <img src="https://cdn.mos.cms.futurecdn.net/6Kowst46WKuB7BzkPs7Pcn-1920-80.jpg.webp" alt="landscape zone"></img> */}
       </div>
       <div className="design-content">
         <div className="indoor-plant">
           <div>
-            <h2>Favourite Indoor Plants</h2>
+            <h2>Indoor Plants</h2>
             <img src="https://houseplantcentral.com/wp-content/uploads/2022/08/20-indoor-trees.jpg" alt="low-light plants"></img>
           </div>                   
         </div>
         </div>
         
           <div className="outdoor-plant">          
-            <h2>Outdoor Landscape Layout</h2>
+            <h2>Outdoor Landscape Design</h2>
         </div>
       </div>
     
